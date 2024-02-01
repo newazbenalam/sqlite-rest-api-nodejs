6fc50fdf374657a8caa19ecc2811056d863db406
newazbenalam <newazbenalam@gmail.com>
2024-02-01 15:01:23 +0600

    add .env path

--- app/models/db.js
+++ app/models/db.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
+  dialect: process.env.DB_TYPE, // (e.g., 'sqlite', 'mysql', 'postgres', etc.)
+  storage: process.env.DB_PATH, 
 });
 
 module.exports = sequelize;
