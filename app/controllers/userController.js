d493e9502ad233fe1e12cb9ce09c5628a8d79a46
newazbenalam <newazbenalam@gmail.com>
2024-02-01 15:01:08 +0600

    custom status

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,7 +3,7 @@ const { User } = require('../models');
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.findAll();
-    res.json(users);
+    res.status(200).json(users);
   } catch (error) {
     console.error('Error fetching users:', error);
     res.status(500).json({ error: 'Internal server error' });
